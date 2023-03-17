ca8b9814120561024d4cadc4c9d9f8f24c7d9c4f
gerbrian93 <65586994+gerbrian93@users.noreply.github.com>
2023-03-17 19:57:38 -0400

    Added while loop to go through each line.

--- project1.c
+++ project1.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
    FILE *fp;
-   char line[250];
+   char line[50];
+   char j;
    fp = fopen("students.asm", "r");
    if (fp == NULL)
    {
       perror("error opening file");
       return 1;
    }
-   printf("File opened");
-   fread(line, sizeof(char), 25, fp);
-   printf("%s", line);
-   fclose(fp);
+   // char *opcode, *rs, *rt, *rd, *shamt, *funct;
+   // j = 1;
+   char *token;
+   while (fgets(line, sizeof(line), fp) != NULL) // this loop goes through each line of the program
+   {
+      // printf(line);
+      //   We'll need the first loop to parse the line and convert to machine code
+      token = strtok(line, " ");
+      // write to out file.
+      // while (token != NULL)
+      //{
+      printf("%s", token);
+      token = strtok(NULL, "");
+      //}
+   }
 }
