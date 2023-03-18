0933da94d0f7256301fd98b9a81d559745719d89
gerbrian93 <65586994+gerbrian93@users.noreply.github.com>
2023-03-17 22:03:11 -0400

    most recent update

--- project1.c
+++ project1.c
@@ -3,28 +3,33 @@
 
 int main()
 {
-   FILE *fp;
-   char line[50];
-   char j;
+   FILE *fp, *outfp;
+   char buffer[200];
+   char *opcode, *rs, *rt, *rd, *shamt, *funct;
+
    fp = fopen("students.asm", "r");
-   if (fp == NULL)
+   outfp = fopen("students2.out", "wb");
+
+   if (fp == NULL || outfp == NULL)
    {
       perror("error opening file");
       return 1;
    }
-   // char *opcode, *rs, *rt, *rd, *shamt, *funct;
-   // j = 1;
+
    char *token;
-   while (fgets(line, sizeof(line), fp) != NULL) // this loop goes through each line of the program
+
+   while (fgets(buffer, sizeof(buffer), fp) != NULL) // this loop goes through each line of the program
    {
       // printf(line);
       //   We'll need the first loop to parse the line and convert to machine code
-      token = strtok(line, " ");
+      token = strtok(buffer, " ,");
+      while (token != NULL)
+      {
+         printf("%s\n", token);
+         token = strtok(NULL, " ,");
+      }
       // write to out file.
-      // while (token != NULL)
-      //{
-      printf("%s", token);
-      token = strtok(NULL, "");
-      //}
    }
+   // content = fread(buffer, sizeof(char), 200, fp);
+   fclose(fp);
 }
