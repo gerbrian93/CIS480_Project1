4f77b69a6bfa8dbad3f69fcb94263cf92ac370d9
gerbrian93 <65586994+gerbrian93@users.noreply.github.com>
2023-03-18 00:07:53 -0400

    Newest push!

--- project1.c
+++ project1.c
@@ -6,7 +6,7 @@ int main()
    FILE *fp, *outfp;
    char buffer[200];
    char *opcode, *rs, *rt, *rd, *shamt, *funct;
-
+   int data = 0, text = 0;
    fp = fopen("students.asm", "r");
    outfp = fopen("students2.out", "wb");
 
@@ -22,14 +22,35 @@ int main()
    {
       // printf(line);
       //   We'll need the first loop to parse the line and convert to machine code
-      token = strtok(buffer, " ,\n");
+      token = strtok(buffer, " ,");
       while (token != NULL)
       {
-         if (token == NULL || token[0] == '#')
+         if (strcmp(token, ".text") == 0)
          {
-            printf("%s\n", token);
+            // add code
+            text = 0;
+            data = 1;
          }
-         token = strtok(NULL, " ,\n");
+         else if (strcmp(token, ".data") == 0)
+         {
+            // add code
+            data = 1;
+            text = 0;
+         }
+         else if (strcmp(token, ".dbyte") == 0)
+         {
+            // add code
+         }
+         else if (strcmp(token, ".integer") == 0)
+         {
+            // add code
+         }
+         else
+         {
+            // R-type rtype(token)
+            // I-type
+         }
+         token = strtok(NULL, " ,");
       }
       // write to out file.
    }
